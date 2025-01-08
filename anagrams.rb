b36023a16526cf7077afe1aa26daed3d2e985036
winstonbarbe <winston.barbe@gmail.com>
2025-01-07 19:45:38 -0500

    adds if logic to funtion to deteremine if it's possible for an anagram to exist

--- anagrams.rb
+++ anagrams.rb
@@ -1,3 +1,16 @@
 def match(word, list)
   # Your job is to complete the code for this method!
+  list.each do |item|
+    could_be_anagram = true
+    if item.length == word.length
+      item.split("").map do |char|
+        p char
+      end
+    else
+      could_be_anagram = false
+    end
+    p could_be_anagram
+  end
 end
+
+match("diaper", ["hello", "worlds", "zombies", "pants"])
